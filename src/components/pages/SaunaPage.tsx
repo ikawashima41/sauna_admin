2908ba90e999a28d2462f759778144d4b29a68f9
ikawashima41 <i.kawashima41@gmail.com>
2021-05-03 18:13:53 +0900

    create base page

---
+++ src/components/pages/SaunaPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GenericTemplate from "../templates/GenericTemplate";
+
+const SaunaPage: React.FC = () => {
+    return (
+        <GenericTemplate title="サウナページ">
+            <>サウナ詳細</>
+        </GenericTemplate>
+    );
+};
+
+export default SaunaPage
